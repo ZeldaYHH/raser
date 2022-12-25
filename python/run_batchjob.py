f7355e24e132a84a7f39c84cb90cc2b8cfdf433e
heheda2001123 <1256257282@qq.com>
2022-12-25 10:34:15 +0800

    Update run_batchjob.py

--- python/run_batchjob.py
+++ python/run_batchjob.py
@@ -22,7 +22,7 @@ def main():
     command_name = command.replace(" ","_").replace("/","_")
     jobfile_name = "./output/{}/jobs/".format(destination_subfolder)+command_name+".job"
     gen_job(jobfile_name,run_code="raser \"./python/"+command+"\"")
-    # command: raser "./python/xxx.py parameters"
+    # run code: raser "./python/xxx.py parameters"
     submit_job(jobfile_name,destination_subfolder,group)
 
 def gen_job(jobfile_name,run_code):
