eb45ffa6da1def7fe6390b6b71cde690d56ca989
fuchenxi <1256257282@qq.com>
2023-02-15 09:19:38 +0800

    p4画图与程序格式调整

--- python/tct_analysis.py
+++ python/tct_analysis.py
@@ -18,51 +18,57 @@ import numpy as np
 import drawsave
 
 def main():
-   path=sys.argv[1]
-   drawsave.create_path(path)
-   Z= array("d")
-   amplitude= array("d")
-   risetime= array("d")
-   charge= array("d")
-   elefield= array("d")
-   sum_k=0
-   sum_l=0
+    path=sys.argv[1]
+    drawsave.create_path(path)
+    Z= array("d")
+    amplitude= array("d")
+    risetime= array("d")
+    charge= array("d")
+    elefield= array("d")
+    sum_k=0
+    sum_l=0
 
-   for L in range(51):
-      volt=array("d",[0.])
-      time=array("d",[0.])
-      Z.append(L)     
-      rootfile=path + "sim-TCT"+str(L)+".root"
-      print(str(rootfile))
-      volt,time=read_rootfile(rootfile)
-      mean=0
-      J=len(volt)
-      amplitude.append(max(volt))
-      k,l=get_average(volt,time,J,mean)
-      sum_k+=k      
-      sum_l+=l
-   k=int(round(np.true_divide(sum_k,51)))
-   l=int(round(np.true_divide(sum_l,51)))
+    for L in range(51):
+        volt=array("d",[0.])
+        time=array("d",[0.])
+        Z.append(L)     
+        rootfile=path + "sim-TCT"+str(L)+".root"
+        print(str(rootfile))
+        volt,time=read_rootfile(rootfile)
+        mean=0
+        J=len(volt)
+        amplitude.append(max(volt))
+        k,l=get_average(volt,time,J,mean)
+        sum_k+=k      
+        sum_l+=l
+    k=int(round(np.true_divide(sum_k,51)))
+    l=int(round(np.true_divide(sum_l,51)))
+    print(k,l)
 
-   for L in range(51):
-      volt=array("d",[0.])
-      time=array("d",[0.])
-      sum_v=0
-      rootfile=path + "sim-TCT"+str(L)+".root"
-      volt,time=read_rootfile(rootfile)
-      J=len(volt)
-      field=get_elefield(volt,k,l)
-      elefield.append(field)
-      cha=get_charge(volt,J)
-      charge.append(cha)
-      rt=get_risetime(volt,time,J,mean)
-      risetime.append(rt)  
+    for L in range(51):
+        volt=array("d",[0.])
+        time=array("d",[0.])
+        sum_v=0
+        rootfile=path + "sim-TCT"+str(L)+".root"
+        volt,time=read_rootfile(rootfile)
+        J=len(volt)
+        field=get_elefield(volt,k,l)
+        elefield.append(field)
+        cha=get_charge(volt,J)
+        charge.append(cha)
+        rt=get_risetime(volt,time,J,mean)
+        risetime.append(rt)  
+
+    print(amplitude)
+    print(charge)
+    print(elefield)
+    print(risetime)
       
-   draw_graphs(amplitude,Z,"Amplitude",path)
-   draw_graphs(charge,Z,"Charge",path)
-   draw_graphs(risetime,Z,"RiseTime",path)
-   draw_graphs(elefield,Z,"Elefield",path)
-   return
+    draw_graphs(amplitude,Z,"Amplitude",path)
+    draw_graphs(charge,Z,"Charge",path)
+    draw_graphs(risetime,Z,"RiseTime",path)
+    draw_graphs(elefield,Z,"Elefield",path)
+    return
 
 def read_rootfile(rootfile):
     J=0
@@ -74,7 +80,7 @@ def read_rootfile(rootfile):
        v1.append(entry.volt)
        t1.append(1000000000*entry.time)
        J=J+1
-    return v1,t1    
+    return v1,t1
 
 def add_noise(rootfile,J,v1,t1):
     v2=array("d",[0.])
@@ -94,17 +100,17 @@ def add_noise(rootfile,J,v1,t1):
 def get_average(volt,time,J,mean):
     Vmax=max(volt)
     for k in range(1,J):
-      if (volt[k-1] - mean)<0.4*(Vmax-mean)<(volt[k] - mean) and time[k]>0.5:
-         break
+        if (volt[k-1] - mean)<0.4*(Vmax-mean)<(volt[k] - mean):
+            break
     for l in range(J-1):
-      if (volt[l] - mean)<0.6*(Vmax - mean)<(volt[l+1] - mean):
-         break
+        if (volt[l] - mean)<0.6*(Vmax - mean)<(volt[l+1] - mean):
+            break
     return k,l 
 
 def get_elefield(volt,k,l):
     sum_volt=0
     for j in range(k,l+1):
-         sum_volt+=volt[j]    
+        sum_volt+=volt[j]
     return sum_volt
 
 def get_charge(volt,J):
@@ -118,15 +124,16 @@ def get_risetime(volt,time,J,mean):
     y=array("d")
     Vmax=max(volt)
     for k in range(1,J):
-      if (volt[k-1] - mean)<0.2*(Vmax-mean)<(volt[k] - mean) and time[k]>0.5:
-         break
+        if (volt[k-1] - mean)<0.2*(Vmax-mean)<(volt[k] - mean):
+            break
     for l in range(J-1):
-      if (volt[l] - mean)<0.8*(Vmax - mean)<(volt[l+1] - mean):
-         break
+        if (volt[l] - mean)<0.8*(Vmax - mean)<(volt[l+1] - mean):
+            break
     n=l-k+1
     for j in range(k,l+1):
-         x.append(time[j])
-         y.append(volt[j])
+        x.append(time[j])
+        y.append(volt[j])
+
     graph1 = ROOT.TGraph(n,x,y)
     f=ROOT.TF1("f","[0]+[1]*x",0,2)
     graph1.Fit(f)
