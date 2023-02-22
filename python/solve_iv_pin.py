af20bf744fefae677bb0db92fe6ba1ce41579c08
lizaiyi <2751013826@qq.com>
2023-02-22 19:08:25 +0800

    增加缺陷参数和求解跳过不收敛的点

--- python/solve_iv_pin.py
+++ python/solve_iv_pin.py
@@ -4,6 +4,7 @@
 import devsim 
 import os
 import sys
+import math
 
 from raser import Physics
 from raser import Node
@@ -35,6 +36,36 @@ nju_pin_5mm_5mm_mesh.SetDoping(device=device, region=region)
 nju_pin_5mm_5mm_mesh.Draw_Doping(device=device, region=region, path="./output/devsim/nju_pin_doping.png")
 
 devsim.open_db(filename="./output/devsim/SICARDB", permission="readonly")
+
+#add defect parameters
+N_c=3.25e15 #effective density of states in conduction band
+N_v=4.8e15 #effective density of states in valence band
+k=1.3806503e-23 
+T0=300
+#Z1/2
+E_t11=-0.67*1.6e-19 #J
+E_t12=-2.56*1.6e-19
+n_11=N_c*math.exp(E_t11/(k*T0))
+p_11=N_v*math.exp(E_t12/(k*T0))
+devsim.set_parameter(device=device,region=region,name="N_t1",value=2e15)#density of Z1/2,4.1e13 from paper
+devsim.set_parameter(device=device,region=region,name="r_n1",value=2e-7)#electron capture constant of Z1/2
+devsim.set_parameter(device=device,region=region,name="r_p1",value=3e-7)#hole capture constant of Z1/2
+devsim.set_parameter(device=device,region=region,name="E_t11",value=E_t11)#Z1/2 Et-Ec
+devsim.set_parameter(device=device,region=region,name="E_t12",value=E_t12)#Z1/2 -(Et-Ev)
+devsim.set_parameter(device=device,region=region,name="n_11",value=n_11)#n1 of Z1/2
+devsim.set_parameter(device=device,region=region,name="p_11",value=p_11)#p1 of Z1/2
+#EH6/7
+E_t21=-1.65*1.6e-19 #J
+E_t22=-1.58*1.6e-19
+n_12=N_c*math.exp(E_t21/(k*T0))
+p_12=N_v*math.exp(E_t22/(k*T0))
+devsim.set_parameter(device=device,region=region,name="N_t2",value=0)#density of EH6/7,3.9e13 from paper
+devsim.set_parameter(device=device,region=region,name="r_n2",value=2.4e-7)#electron capture constant of EH6/7
+devsim.set_parameter(device=device,region=region,name="r_p2",value=5e-11)#hole capture constant of EH6/7
+devsim.set_parameter(device=device,region=region,name="E_t21",value=E_t21)#EH6/7 Et-Ec
+devsim.set_parameter(device=device,region=region,name="E_t22",value=E_t22)#EH6/7 -(Et-Ev)
+devsim.set_parameter(device=device,region=region,name="n_12",value=n_12)#n1 of EH6/7
+devsim.set_parameter(device=device,region=region,name="p_12",value=p_12)#p1 of EH6/7
 
 # Extended precision
 devsim.set_parameter(name = "extended_solver", value=True)
@@ -72,7 +103,11 @@ ax1 = fig1.add_subplot(111)
 while reverse_v < 800.0:
 
     devsim.set_parameter(device=device, name=Physics.GetContactBiasName("top"), value=0-reverse_v)
-    devsim.solve(type="dc", absolute_error=1e10, relative_error=1e-10, maximum_iterations=50)
+    try:
+        devsim.solve(type="dc", absolute_error=1e10, relative_error=1e-10, maximum_iterations=50)
+    except devsim.error as msg:
+        if msg=="Convergence failure!":
+            raise
     Physics.PrintCurrents(device, "top")
     Physics.PrintCurrents(device, "bot")
     reverse_top_electron_current= devsim.get_contact_current(device=device, contact="top", equation="ElectronContinuityEquation")
