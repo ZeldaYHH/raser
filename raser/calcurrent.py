21b3201ef709a851ab602220fca7dda65015e1d9
Wang-unknown <1365447033@qq.com>
2023-04-10 11:26:45 +0800

    update

--- raser/calcurrent.py
+++ raser/calcurrent.py
@@ -138,7 +138,7 @@ class Carrier:
                 q = self.charge * e0
                 dU_w = U_w_2 - U_w_1
                 self.signal[j].append(q*dU_w)
-
+        
 
     def drift_end(self,my_f):
         e_field = my_f.get_e_field(self.d_x,self.d_y,self.d_z)
