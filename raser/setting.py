e20614d9004eec638889dd410a186799c19fcd72
Xingchen Li <1902370441@qq.com>
2023-04-18 21:41:47 +0800

    rebuild geant4 part

--- raser/setting.py
+++ raser/setting.py
@@ -33,10 +33,15 @@ class Setting:
         self.input2dic(parameters)
         self.det_name = self._pardic['det_name']
         self.read_par(self._pardic['parfile'])
+
         if "laser_model" in self._pardic:
             self.laser_model = self._pardic['laser_model']
             self.read_par_laser(self._pardic['laser_parfile'])
-        
+
+        if "geant4_model" in self._pardic:
+            self.geant4_model = self._pardic['geant4_model']
+            self.read_par_geant4(self._pardic['geant4_parfile'])
+
         p = self.paras
         self.total_events = int(p['total_events'])
         #self.g4seed = 0 
@@ -76,6 +81,27 @@ class Setting:
             else:
                 laser_paras[x] = laser_paras[x]
         self.laser_paras = laser_paras
+
+    def read_par_geant4(self,jsonfile):
+        with open(jsonfile) as f:
+            dic_pars = json.load(f)
+        for dic_par in dic_pars:
+            if dic_par['geant4_model'] in self.geant4_model:
+                geant4_paras = dic_par
+        for x in geant4_paras: 
+            if self.is_number(geant4_paras[x]):          
+                geant4_paras[x] = float(geant4_paras[x])
+            else:
+                geant4_paras[x] = geant4_paras[x]
+        for y in geant4_paras['object']:
+            for z in geant4_paras['object'][y]:
+                for k in geant4_paras['object'][y][z]:
+                    if self.is_number(geant4_paras['object'][y][z][k]):
+                        geant4_paras['object'][y][z][k] = float(geant4_paras['object'][y][z][k])
+                    else:
+                        geant4_paras['object'][y][z][k] = geant4_paras['object'][y][z][k]
+        self.geant4_paras = geant4_paras
+
 
     @property
     def detector(self):
@@ -218,12 +244,15 @@ class Setting:
         ---------
             2021/09/02
         """
-        p = self.paras
+        p = self.geant4_paras
         pygeant4 = {'det_model':self.det_model,
                     'maxstep':p['maxstep'], 'g4_vis':p['g4_vis'],
                     'par_in':[p['par_inx'], p['par_iny'], p['par_inz']], 
                     "par_out":[p['par_outx'], p['par_outy'], p['par_outz']],
+                    "par_type":p['par_type'], "par_energy":p['par_energy'],
+                    "world":p['world'], "object":p['object'],'model':p['geant4_model']
                     }
+        
         return pygeant4
 
     @property
@@ -332,7 +361,7 @@ class Setting:
         try:
             float(s)
             return True
-        except ValueError:
+        except (TypeError, ValueError):
             pass
         try:
             import unicodedata
