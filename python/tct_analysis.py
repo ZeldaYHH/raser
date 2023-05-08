c66e8a419391b8041e5055cefb62f04d112d8d65
fuchenxi <1256257282@qq.com>
2023-05-08 18:51:58 +0800

    TCT结果比较

--- python/tct_analysis.py
+++ python/tct_analysis.py
@@ -28,12 +28,15 @@ def main():
     # the pulse energy difference in experiment
 
     if "experiment" in sys.argv:
-        amplitude, charge, risetime, elefield, Z = collect_data(path, "sim-TCT", pulse_energy_scale, 1e9)
-        amplitude_exp, charge_exp, risetime_exp, elefield_exp, Z_exp = collect_data(path, "exp-TCT", 1, 1)
+        amplitude, charge, risetime, elefield, Z ,volts, times= collect_data(path, "sim-TCT", pulse_energy_scale, 1e9)
+        amplitude_exp, charge_exp, risetime_exp, elefield_exp, Z_exp, volts_exp, times_exp= collect_data(path, "exp-TCT", 1, 1)
         draw_double_graphs(amplitude,amplitude_exp,Z,"Amplitude",output_path)
         draw_double_graphs(charge,charge_exp,Z,"Charge",output_path)
         draw_double_graphs(risetime,risetime_exp,Z,"RiseTime",output_path)
         draw_double_graphs(elefield,elefield_exp,Z,"Elefield",output_path)
+
+        for volt,time,volt_exp,time_exp,z in zip(volts, times, volts_exp, times_exp, list(Z)):
+            draw_double_signals(time,time_exp,volt,volt_exp,z,path)
 
     else:
         amplitude, charge, risetime, elefield, Z = collect_data(path, "sim-TCT", pulse_energy_scale, 1e9)
@@ -48,6 +51,8 @@ def collect_data(path, model, volt_scale, time_scale):
     risetime= array("d")
     charge= array("d")
     elefield= array("d")
+    volts = []
+    times = []
     sum_k=0
     sum_l=0
 
@@ -75,6 +80,8 @@ def collect_data(path, model, volt_scale, time_scale):
         sum_v=0
         rootfile=path+model+str(rel_z)+".root"
         volt,time=read_rootfile(rootfile,volt_scale, time_scale)
+        volts.append(volt)
+        times.append(time)
         J=len(volt)
         field=get_elefield(volt,k,l)
         elefield.append(field)
@@ -83,7 +90,7 @@ def collect_data(path, model, volt_scale, time_scale):
         rt=get_risetime(volt,time,J,mean)
         risetime.append(rt)  
       
-    return amplitude, charge, risetime, elefield, Z
+    return amplitude, charge, risetime, elefield, Z, volts, times
 
 def read_rootfile(rootfile,volt_scale,time_scale):
     J=0
@@ -261,6 +268,55 @@ def draw_double_graphs(array1,array2,Z,name,path):
     legend.Draw()
 
     c.SaveAs(path+name+"_comparison.pdf")
+
+def draw_double_signals(time_1,time_2,signal_1,signal_2,z,path):
+    c = ROOT.TCanvas('c', '', 800, 600)
+    c.SetFillColor(0)
+    c.SetFrameFillColor(0)
+    ROOT.gStyle.SetPadColor(0)
+    ROOT.gStyle.SetCanvasColor(0)
+    ROOT.gStyle.SetOptStat(0)
+    c.SetLeftMargin(0.15)
+    c.SetBottomMargin(0.15)
+
+    mg=ROOT.TMultiGraph("mg","")
+    n1=len(time_1)
+    graph1 = ROOT.TGraph(n1,time_1,signal_1)
+    n2=len(time_2)
+    graph2 = ROOT.TGraph(n2,time_2,signal_2)
+
+    graph1.SetLineColor(2)
+    graph2.SetLineColor(1)
+    graph1.SetMarkerColor(2)
+    graph2.SetMarkerColor(1)
+    graph1.SetMarkerStyle(26)
+    graph2.SetMarkerStyle(4)
+
+    mg.Add(graph1)
+    mg.Add(graph2)
+    mg.Draw('ap')
+    
+    mg.GetYaxis().SetTitle('signal [mV]')
+    mg.GetXaxis().SetTitle('time [ns]')
+    mg.GetYaxis().SetLabelSize(0.05)
+    mg.GetYaxis().SetTitleSize(0.05)
+    mg.GetXaxis().SetLabelSize(0.05)
+    mg.GetXaxis().SetTitleSize(0.05)
+
+    mg.GetXaxis().SetLimits(0,10)
+
+    legend = ROOT.TLegend(0.45,0.65, 0.81, 0.86)
+    legend.AddEntry(graph1, "RASER simulation", "p")
+    legend.AddEntry(graph2, "TCT measurement", "p")
+    legend.SetTextSize(27)
+    legend.SetTextFont(43)
+
+    legend.SetBorderSize(0)
+    legend.SetFillColor(0)
+    legend.Draw()
+
+    c.SaveAs(path+str(z)+"_comparison.pdf")
+
 
 if __name__ == "__main__":
     main()
