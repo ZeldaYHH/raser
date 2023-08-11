fd8d0cbfd12e099e7d9273f9ff91f179e1404536
senzhao <tokyosenzhao@163.com>
2023-08-11 12:07:47 +0800

    change name and 1.3.21push

--- python/device_2d.py
+++ python/device_2d.py
@@ -15,18 +15,7 @@ import matplotlib.pyplot
 if not (os.path.exists("./output/testdio")):
     os.mkdir("./output/testdio")
 
-#####
-# dio1
-#
-# Make doping a step function
-# print dat to text file for viewing in grace
-# verify currents analytically
-# in dio2 add recombination
-#
 
-####
-#### Meshing
-####
 
 type=sys.argv[1]
 
@@ -51,61 +40,12 @@ def createMesh(device, region):
     devsim.finalize_mesh   (mesh="dog")
     devsim.create_device   (mesh="dog", device=device)
 
-'''
-def Create2DMesh(device,region):
-    devsim.create_2d_mesh  (mesh="Sicar")
-    #宽0.01cm
-    devsim.add_2d_mesh_line(mesh="Sicar", dir="x", pos=0     , ps=1e-3)
-    devsim.add_2d_mesh_line(mesh="Sicar", dir="x", pos=1e-2  , ps=1e-3)
-
-    #长0.5cm
-    devsim.add_2d_mesh_line(mesh="Sicar", dir="y", pos=0     , ps=1e-3)
-    devsim.add_2d_mesh_line(mesh="Sicar", dir="y", pos=1e-2 , ps=1e-3)
-
-    #划分各个区域
-    #0-0.3um P++
-    devsim.add_2d_mesh_line(mesh="Sicar", dir="x", pos=0.3e-4     , ps=1e-4)
-    #0.3e-5-1.3um N+
-    devsim.add_2d_mesh_line(mesh="Sicar", dir="x", pos=1.3e-4  , ps=1e-4)
-    #1.3-51.3um n-
-    devsim.add_2d_mesh_line(mesh="Sicar", dir="x", pos=51.3e-4  , ps=1e-4)
-    #51.3um-56.3um nbuffer
-    devsim.add_2d_mesh_line(mesh="Sicar", dir="x", pos=56.3e-4  , ps=1e-4)
-
-    devsim.add_2d_mesh_line(mesh="Sicar", dir="x", pos=-1e-4   , ps=1e-4)
-    devsim.add_2d_mesh_line(mesh="Sicar", dir="x", pos=1.01e-2, ps=1e-4)
-
-    devsim.add_2d_region   (mesh="Sicar", material="SiliconCarbide", region=region)
-    devsim.add_2d_region   (mesh="Sicar", material="SiliconCarbide", region="air1", xl=-1e-4,  xh=0)
-    devsim.add_2d_region   (mesh="Sicar", material="SiliconCarbide", region="air2", xl=1.01e-2, xh=1e-2)
-
-    devsim.add_2d_contact  (mesh="Sicar", name="top", region=region, yl=0, yh=0.5, xl=0, xh=0, bloat=1e-6, material="metal")
-    devsim.add_2d_contact  (mesh="Sicar", name="bot", region=region, xl=1e-2,   xh=1e-2, bloat=1e-6, material="metal")
-
-    devsim.finalize_mesh   (mesh="Sicar")
-    devsim.create_device   (mesh="Sicar", device=device)
-
-
-
-
-
-
-
-'''
-
 
 
 device="MyDevice"
 region="MyRegion"
 
-if type =="pn":
-    
-    createMesh(device, region)
-else:
-     print("1.3.20 is used to simulate simple pn junction,please input pn to run")
-
-
-
+createMesh(device,region)
 
 
 dio2_element_physics.setMaterialParameters(device, region)
@@ -116,17 +56,24 @@ dio2_element_physics.createSolution(device, region, "Potential")
 #### NetDoping
 ####
 
+def NetDoping(device=device, region=region):
+    if type=="PNjuction":
 
-for name, equation in (
-    ("Acceptors", "2.0e19*step(0.3e-7-x)"),
-  ("Donors",    "1.0e18*step(x-3e-8)"),
-  ("NetDoping", "Donors-Acceptors"),
-):
-    devsim.node_model(device=device, region=region, name=name, equation=equation)
+        for name, equation in (
+        ("Acceptors", "2.0e19*step(0.3e-7-x)"),
+        ("Donors",    "1.0e18*step(x-(3e-8))"),
+        ("NetDoping", "Donors-Acceptors"),
+        ):
+            devsim.node_model(device=device, region=region, name=name, equation=equation)
+    elif type=="PNwithGainlayer":
+        for name, equation in (
+        ("Acceptors", "2.0e20*step(0.3e-7-x)"),
+        ("Donors",    "1.80e17*( step((1.3e-7)-x) -step((3e-8)-x) ) + 1.0e14*( step((51.3e-7)-x) - step((1.3e-7)-x) ) + 1.0e18*( step((56.3e-7)-x) - step((51.3e-7)-x) )+ 2.0e18*( step((66.3e-7)-x) - step((56.3e-7)-x) )"),
+        ("NetDoping", "Donors-Acceptors"),
+        ):
+            devsim.node_model(device=device, region=region, name=name, equation=equation)
 
-
-
-
+NetDoping(device,region)
 
 
 dio2_element_physics.createPotentialOnly(device, region)
@@ -168,13 +115,6 @@ devsim.set_parameter(device=device, region=region, name="top", value=0.0)
 devsim.circuit_element(name="V1", n1=1,         n2=0, value=1.0, acreal=1.0)
 devsim.circuit_element(name="R1", n1="top", n2=1, value=1e3)
 
-
-#
-# This is to test the solution backup
-#
-#set_parameter -device device -region region -name "topbias" -value 10
-#catch {solve -type dc -absolute_error 1e10 -relative_error 1e-10 -maximum_iterations 30} x
-#puts x
 
 v=0
 while v < 7:
