fe6fc959c49aa0060895afe13caa426072d3959b
Xin Shi <shixin@ihep.ac.cn>
2023-09-11 16:20:56 +0800

    update setup

--- cfg/setup.sh
+++ cfg/setup.sh
@@ -6,7 +6,7 @@ echo "Setting up raser ..."
 
 export PATH=/afs/ihep.ac.cn/soft/common/sysgroup/hep_job/bin:$PATH
 export IMGFILE=/afs/ihep.ac.cn/users/s/shixin/raser/raser-2.0.sif
-export BINDPATH=/cefs,/afs,/besfs5,/cvmfs,/scratchfs,/workfs2
+export BINDPATH=/cefs,/afs,/besfs5,/cvmfs,/scratchfs,/workfs2,/publicfs
 
 alias raser="apptainer exec --env-file cfg/env -B $BINDPATH $IMGFILE python3 raser"
 alias test-raser="apptainer exec --env-file cfg/env -B $BINDPATH $IMGFILE python3 -m unittest discover -v -s tests"
