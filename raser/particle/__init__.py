8d262a0c3f42ce395a3c534f2cf85a8eb738713e
heye <heye@ihep.ac.cn>
2023-09-28 19:39:39 +0800

    init

--- raser/particle/__init__.py
+++ raser/particle/__init__.py
@@ -4,9 +4,7 @@ from . import calculate_temperature
 def main(args):
     label = vars(args)['label']
 
-    if label == 'foo':
-        foo.main()
-    elif label == 'temperature':
+    if label == 'temperature':
         calculate_temperature.main()
     else:
         raise NameError(label)
