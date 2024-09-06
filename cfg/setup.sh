40ee214bbcdcd0cf37969c3e69d0ac9939d62590
fuchenxi <1256257282@qq.com>
2024-09-06 16:19:22 +0800

    修正输出路径 .gitignore整理 setup提取出Geant4PATH

--- cfg/setup.sh
+++ cfg/setup.sh
@@ -5,6 +5,8 @@
 echo "Setting up raser ..."
 
 dir_raser=$(cd $(dirname $(dirname $BASH_SOURCE[0])) && pwd)
+Geant4PATH=/cvmfs/common.ihep.ac.cn/software/geant4/10.7.p02
+
 cfg_env=$dir_raser/cfg/env
 rm -f $cfg_env
 cat << EOF >> $cfg_env
@@ -15,22 +17,22 @@ PATH=/cvmfs/common.ihep.ac.cn/software/hepjob/bin:/usr/bin:\$PATH
 ROOTSYS=/usr/local/share/root_install
 
 # Geant4 
-G4ENSDFSTATEDATA=/cvmfs/common.ihep.ac.cn/software/geant4/10.7.p02/data/G4ENSDFSTATE2.3
-G4PIIDATA=/cvmfs/common.ihep.ac.cn/software/geant4/10.7.p02/data/G4PII1.3
-G4INCLDATA=/cvmfs/common.ihep.ac.cn/software/geant4/10.7.p02/data/G4INCL1.0
-G4LEDATA=/cvmfs/common.ihep.ac.cn/software/geant4/10.7.p02/data/G4EMLOW7.13
-G4PARTICLEXSDATA=/cvmfs/common.ihep.ac.cn/software/geant4/10.7.p02/data/G4PARTICLEXS3.1.1
-GEANT4_INSTALL=/cvmfs/common.ihep.ac.cn/software/geant4/10.7.p02/install
-G4NEUTRONHPDATA=/cvmfs/common.ihep.ac.cn/software/geant4/10.7.p02/data/G4NDL4.6
-G4SAIDXSDATA=/cvmfs/common.ihep.ac.cn/software/geant4/10.7.p02/data/G4SAIDDATA2.0
-G4REALSURFACEDATA=/cvmfs/common.ihep.ac.cn/software/geant4/10.7.p02/data/RealSurface2.2
-G4ABLADATA=/cvmfs/common.ihep.ac.cn/software/geant4/10.7.p02/data/G4ABLA3.1
-G4LEVELGAMMADATA=/cvmfs/common.ihep.ac.cn/software/geant4/10.7.p02/data/PhotonEvaporation5.7
-G4RADIOACTIVEDATA=/cvmfs/common.ihep.ac.cn/software/geant4/10.7.p02/data/RadioactiveDecay5.6
+GEANT4_INSTALL=$Geant4PATH/install
+G4ENSDFSTATEDATA=$Geant4PATH/data/G4ENSDFSTATE2.3
+G4PIIDATA=$Geant4PATH/data/G4PII1.3
+G4INCLDATA=$Geant4PATH/data/G4INCL1.0
+G4LEDATA=$Geant4PATH/data/G4EMLOW7.13
+G4PARTICLEXSDATA=$Geant4PATH/data/G4PARTICLEXS3.1.1
+G4NEUTRONHPDATA=$Geant4PATH/data/G4NDL4.6
+G4SAIDXSDATA=$Geant4PATH/data/G4SAIDDATA2.0
+G4REALSURFACEDATA=$Geant4PATH/data/RealSurface2.2
+G4ABLADATA=$Geant4PATH/data/G4ABLA3.1
+G4LEVELGAMMADATA=$Geant4PATH/data/PhotonEvaporation5.7
+G4RADIOACTIVEDATA=$Geant4PATH/data/RadioactiveDecay5.6
 
 # Python 
-PYTHONPATH=$dir_raser/raser:/usr/local/share/root_install/lib:/cvmfs/common.ihep.ac.cn/software/geant4/10.7.p02/install/lib64/python3.6/site-packages:/usr/local/share/acts_build/python
-LD_LIBRARY_PATH=/cvmfs/common.ihep.ac.cn/software/geant4/10.7.p02/install/lib64:/usr/local/share/root_install/lib:/.singularity.d/libs
+PYTHONPATH=$dir_raser/raser:/usr/local/share/root_install/lib:$Geant4PATH/install/lib64/python3.6/site-packages:/usr/local/share/acts_build/python
+LD_LIBRARY_PATH=$Geant4PATH/install/lib64:/usr/local/share/root_install/lib:/.singularity.d/libs
 
 #pyMTL3 Verilator
 PYMTL_VERILATOR_INCLUDE_DIR="/usr/local/share/verilator/include"
