97357238f85e0e3e78b9addbfd54cf95020b9f09
fuchenxi <1256257282@qq.com>
2024-10-12 15:13:34 +0800

    更新CEPC_strip的mesh

--- param_file/mesh/CEPC_strip.py
+++ param_file/mesh/CEPC_strip.py
@@ -6,23 +6,33 @@ geo = gmsh.model.geo
 gmsh.initialize()
 gmsh.model.add("CEPC_strip")
 
-lc_up = 5e-5
-lc_down = 5e-4
-lp = 2e-5
-ln = 2e-5
-ln_well = 1e-4
+lc_top = 3e-5
+lc_bulk = 5e-5
+lc_bottom_mid = 5e-5
+lc_bottom = 3e-5
+
+lp = 1e-5
+ln_up = 1e-5
+ln = 3e-5
+ln_well = 3e-5
 
 # bulk points
 P1 = geo.addPoint(0, 0, 0, lp)
 P2 = geo.addPoint(0, 75.5e-4, 0, lp)
-P3 = geo.addPoint(150e-4, 75.5e-4, 0, lc_down)
-P4 = geo.addPoint(150e-4, 0, 0, lc_down)
+P3 = geo.addPoint(150e-4, 75.5e-4, 0, lc_bottom)
+P4 = geo.addPoint(150e-4, 0, 0, lc_bottom)
 
-P17 = geo.addPoint(147e-4, 75.5e-4, 0, lc_down)
-P18 = geo.addPoint(147e-4, 0, 0, lc_down)
+P17 = geo.addPoint(147e-4, 75.5e-4, 0, lc_bottom_mid)
+P18 = geo.addPoint(147e-4, 0, 0, lc_bottom_mid)
+
+P29 = geo.addPoint(10e-4, 75.5e-4, 0, lc_bulk)
+P30 = geo.addPoint(10e-4, 0, 0, lc_bulk)
+
+P31 = geo.addPoint(140e-4, 75.5e-4, 0, lc_bulk)
+P32 = geo.addPoint(140e-4, 0, 0, lc_bulk)
 
 # p stop 1
-# P1 = geo.addPoint(0, 0, 0, lc_up)
+# P1 = geo.addPoint(0, 0, 0, lc_top)
 P6 = geo.addPoint(0, 1e-4, 0, lp)
 P7 = geo.addPoint(-1e-4, 1e-4, 0, lp)
 P8 = geo.addPoint(-1e-4, 0, 0, lp)
@@ -38,7 +48,7 @@ surf1 = geo.addPlaneSurface([loop1])
 # p well 1
 P19 = geo.addPoint(2e-4, 0, 0, lp)
 P20 = geo.addPoint(2e-4, 1e-4, 0, lp)
-# P1 = geo.addPoint(0, 0, 0, lc_up)
+# P1 = geo.addPoint(0, 0, 0, lc_top)
 # P6 = geo.addPoint(0, 1e-4, 0, lp)
 
 L21 = geo.addLine(P1, P19)
@@ -51,7 +61,7 @@ surf2 = geo.addPlaneSurface([loop2])
 
 # p stop 2
 P9 = geo.addPoint(0,74.5e-4, 0, lp)
-# P2 = geo.addPoint(0,75.5e-4, 0, lc_up)
+# P2 = geo.addPoint(0,75.5e-4, 0, lc_top)
 P11 = geo.addPoint(-1e-4,75.5e-4, 0, lp)
 P12 = geo.addPoint(-1e-4,74.5e-4, 0, lp)
 
@@ -68,7 +78,7 @@ surf3 = geo.addPlaneSurface([loop3])
 P21 = geo.addPoint(2e-4,75.5e-4, 0, lp)
 P22 = geo.addPoint(2e-4,74.5e-4, 0, lp)
 # P9 = geo.addPoint(0,74.5e-4, 0, lp)
-# P2 = geo.addPoint(0,75.5e-4, 0, lc_up)
+# P2 = geo.addPoint(0,75.5e-4, 0, lc_top)
 
 L24 = geo.addLine(P2, P21)
 L25 = geo.addLine(P21, P22)
@@ -79,12 +89,12 @@ loop4 = geo.addCurveLoop([L24, L25, L26, L17])
 surf4 = geo.addPlaneSurface([loop4])
 
 # n stop
-P13 = geo.addPoint(0, 30.25e-4, 0, ln)
-P14 = geo.addPoint(0, 45.25e-4, 0, ln)
-P23 = geo.addPoint(-0.5e-4, 45.25e-4, 0, ln)
-P24 = geo.addPoint(-0.5e-4, 30.25e-4, 0, ln)
-P15 = geo.addPoint(-1e-4, 45.25e-4, 0, ln)
-P16 = geo.addPoint(-1e-4, 30.25e-4, 0, ln)
+P13 = geo.addPoint(0, 30.25e-4, 0, ln_up)
+P14 = geo.addPoint(0, 45.25e-4, 0, ln_up)
+P23 = geo.addPoint(-0.5e-4, 45.25e-4, 0, ln_up)
+P24 = geo.addPoint(-0.5e-4, 30.25e-4, 0, ln_up)
+P15 = geo.addPoint(-1e-4, 45.25e-4, 0, ln_up)
+P16 = geo.addPoint(-1e-4, 30.25e-4, 0, ln_up)
 
 L27 = geo.addLine(P15, P23)
 L28 = geo.addLine(P23, P24)
@@ -108,8 +118,8 @@ P25 = geo.addPoint(2e-4, 46.75e-4, 0, ln_well)
 P26 = geo.addPoint(2e-4, 28.75e-4, 0, ln_well)
 P27 = geo.addPoint(0e-4, 46.75e-4, 0, ln_well)
 P28 = geo.addPoint(0e-4, 28.75e-4, 0, ln_well)
-# P13 = geo.addPoint(0, 30.25e-4, 0, lc_up)
-# P14 = geo.addPoint(0, 45.25e-4, 0, lc_up)
+# P13 = geo.addPoint(0, 30.25e-4, 0, lc_top)
+# P14 = geo.addPoint(0, 45.25e-4, 0, lc_top)
 
 L30 = geo.addLine(P28, P26)
 L31 = geo.addLine(P26, P25)
@@ -132,27 +142,43 @@ L4 = geo.addLine(P6, P28)
 L36 = geo.addLine(P27, P9)
 # L26 = geo.addLine(P22, P9)
 # L25 = geo.addLine(P21, P22)
-L12 = geo.addLine(P21, P17)
-L13 = geo.addLine(P17, P18)
-L14 = geo.addLine(P18, P19)
+L12 = geo.addLine(P21, P29)
+L13 = geo.addLine(P29, P30)
+L14 = geo.addLine(P30, P19)
 
 loop8 = geo.addCurveLoop([L22, L23, L4, L30, L31, L32, L36, -L26, -L25, L12, L13, L14])
 surf8 = geo.addPlaneSurface([loop8])
 
-L18 = geo.addLine(P17, P3)
-L19 = geo.addLine(P3, P4)
-L20 = geo.addLine(P4, P18)
-# L13 = geo.addLine(P17, P18)
+L18 = geo.addLine(P31, P29)
+L19 = geo.addLine(P31, P32)
+L20 = geo.addLine(P30, P32)
+# L13 = geo.addLine(P29, P30)
 
-loop9 = geo.addCurveLoop([L18, L19, L20, -L13])
+loop9 = geo.addCurveLoop([-L18, L19, -L20, -L13])
 surf9 = geo.addPlaneSurface([loop9])
+
+L40 = geo.addLine(P31, P17)
+L41 = geo.addLine(P17, P18)
+L42 = geo.addLine(P18, P32)
+# L19 = geo.addLine(P31, P32)
+
+loop10 = geo.addCurveLoop([L40, L41, L42, -L19])
+surf10 = geo.addPlaneSurface([loop10])
+
+L37 = geo.addLine(P17, P3)
+L38 = geo.addLine(P3, P4)
+L39 = geo.addLine(P4, P18)
+# L41 = geo.addLine(P17, P18)
+
+loop11 = geo.addCurveLoop([L37, L38, L39, -L41])
+surf11 = geo.addPlaneSurface([loop11])
 
 geo.synchronize()
 
-gmsh.model.addPhysicalGroup(1, [L19], name="bot")
+gmsh.model.addPhysicalGroup(1, [L38], name="bot")
 gmsh.model.addPhysicalGroup(1, [L6], name="top")
 
-gmsh.model.addPhysicalGroup(2, [surf1, surf2, surf3, surf4, surf5, surf6, surf7, surf8, surf9], name="CEPC_strip")
+gmsh.model.addPhysicalGroup(2, [surf1, surf2, surf3, surf4, surf5, surf6, surf7, surf8, surf9, surf10, surf11], name="CEPC_strip")
 
 gmsh.option.setNumber("Geometry.MatchMeshTolerance", 1e-12)
 gmsh.model.mesh.generate(2)
